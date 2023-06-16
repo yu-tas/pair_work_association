c3229447ab180141cfda4965a21d28ba56c37a7b
tadao <tadao_kobayashi@diveintocode.jp>
2023-06-16 13:43:28 +0900

    blog関連を削除

--- db/migrate/20230608213503_add_user_ref_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToBlogs < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :blogs, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_08_213503) do
+ActiveRecord::Schema.define(version: 2023_06_08_201653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_blogs_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -32,5 +23,4 @@ ActiveRecord::Schema.define(version: 2023_06_08_213503) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "blogs", "users"
 end
